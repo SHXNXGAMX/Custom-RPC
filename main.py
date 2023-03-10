30df8d73376aea0de27f6b83b5c1df7b2084c02e
SHXNXGAMX <shxnxgamx@mail.ru>
2023-03-10 20:37:15 +0700

    Create save sys

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from pypresence import Presence
 from time import time
+import sqlite3
+
 
 class App(QMainWindow):
 	def __init__(self):
 		super().__init__()
 		uic.loadUi('main.ui', self)
 		self.FieldsStatus = True
+		self.createDb()
 
 		self.ButtonStart.clicked.connect(self.startRPC)
 		self.ButtonStop.clicked.connect(self.stopRPC)
+		self.ButtonLast.clicked.connect(self.loadData)
 
 
 	def startRPC(self):
 		self.createRPC(self.getInputData())
 		self.blockFields()
+		self.dbWrite()
 		self.updateRPC()
 
 	def stopRPC(self):
@@ -44,6 +49,7 @@ class App(QMainWindow):
 		self.ButtonStart.setEnabled(self.FieldsStatus)
 		self.ButtonStop.setEnabled(not self.FieldsStatus)
 		self.ButtonClear.setEnabled(self.FieldsStatus)
+		self.ButtonLast.setEnabled(self.FieldsStatus)
 
 	def getInputData(self) -> dict:
 		options = {
@@ -97,6 +103,85 @@ class App(QMainWindow):
 			small_text = str(options['SmallImageText'])
 		)
 		print('RPC Started!')
+
+	def loadData(self):
+		data = self.dbGet()
+
+		self.ClientID.setText(str(data[0]))
+		self.Details.setText(str(data[1]))
+		self.State.setText(str(data[2]))
+		self.LargeImageKey.setText(str(data[3]))
+		self.LargeImageText.setText(str(data[4]))
+		self.SmallImageKey.setText(str(data[5]))
+		self.SmallImageText.setText(str(data[6]))
+		self.ButtonName1.setText(str(data[7]))
+		self.ButtonUrl1.setText(str(data[8]))
+		self.ButtonName2.setText(str(data[9]))
+		self.ButtonUrl2.setText(str(data[10]))
+		self.Timestamp.setChecked(bool(data[11]))
+
+	def dbWrite(self):
+		
+		sql = """UPDATE 'last_rpc' SET client_id = ?, details = ?, state = ?, large_image_key = ?, large_image_text = ?, small_image_key = ?, small_image_text = ?, button_1_name = ?, button_1_url = ?, button_2_name = ?, button_2_url = ?, "timestamp" = ?;"""
+		options = self.getInputData()
+		values = (
+			options['ClientID'], 
+			options['Details'], 
+			options['State'], 
+			options['LargeImageKey'], 
+			options['LargeImageText'], 
+			options['SmallImageKey'], 
+			options['SmallImageText'], 
+			options['ButtonName1'], 
+			options['ButtonUrl1'], 
+			options['ButtonName2'], 
+			options['ButtonUrl2'], 
+			int(options['Timestamp'])
+			)
+
+		with sqlite3.connect('database.db') as db:
+			cursor = db.cursor()
+			data = cursor.execute(sql, values)
+			db.commit()
+			print(self.dbGet())
+
+	def dbGet(self):
+		sql = """SELECT * FROM 'last_rpc';"""
+
+		with sqlite3.connect('database.db') as db:
+			cursor = db.cursor()
+			data = cursor.execute(sql).fetchone()
+			if data is None:
+				values = ('1083727710901252156', 'CustomRPC UI', 'Lang: Python', 'custom_rpc_avatar', 'CustomRPC', 'shxnxgamx', 'by SHXNXGAMX', 'GitHub', 'https://github.com/SHXNXGAMX/Custom-RPC', '', '', 1)
+				insertSql = """INSERT INTO 'last_rpc'('client_id', 'details', 'state', 'large_image_key', 'large_image_text', 'small_image_key', 'small_image_text', 'button_1_name', 'button_1_url', 'button_2_name', 'button_2_url', 'timestamp') VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+				# 
+
+				cursor.execute(insertSql, values)
+				db.commit()
+
+				data = cursor.execute(sql).fetchone()
+
+		return data
+
+	def createDb(self):
+		sql = """CREATE TABLE IF NOT EXISTS "last_rpc" (
+					"client_id"	TEXT DEFAULT '1083727710901252156',
+					"details"	TEXT,
+					"state"	TEXT,
+					"large_image_key"	TEXT DEFAULT 'custom_rpc_avatar',
+					"large_image_text"	TEXT DEFAULT 'CustomRPC',
+					"small_image_key"	TEXT DEFAULT 'shxnxgamx',
+					"small_image_text"	TEXT DEFAULT 'by SHXNXGAMX',
+					"button_1_name"	TEXT,
+					"button_1_url"	TEXT,
+					"button_2_name"	TEXT,
+					"button_2_url"	TEXT,
+					"timestamp"	INTEGER DEFAULT 1
+				);"""
+		with sqlite3.connect('database.db') as db:
+			cursor = db.cursor()
+			cursor.executescript(sql)
+			db.commit()
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
